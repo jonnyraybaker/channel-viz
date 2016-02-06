522d928b52db5abf8249faf67cf0da8f7b885751
jonnyraybaker <jonny@ubakers.com>
2016-02-05 22:08:52 -0700

    Update follows.js

--- follows.js
+++ follows.js
@@ -16,8 +16,8 @@
 		defaultFeeds	= [2134151370], // Comma separated array of Xively Feed ID numbers
 		applicationName	= 'Home Power Monitoring', // Replaces Xively logo in the header
 		dataDuration	= '12hours', // Default duration of data to be displayed // ref: https://xively.com/dev/docs/api/data/read/historical_data/
-		dataInterval	= 6, // Default interval for data to be displayed (in seconds)
-		dataColor		= '', // CSS HEX value of color to represent data (omit leading #)
+		dataInterval	= 10800, // Default interval for data to be displayed (in seconds)
+		dataColor		= '0A1922', // CSS HEX value of color to represent data (omit leading #)
 		hideForm		= 1; // To hide input form use value of 1, otherwise set to 0
 
 // Function Declarations
